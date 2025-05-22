b524a52d4d6f0f00428a7c5736b9b8fcba4f016f
IanBrenes <iaguilar60911@ufide.ac.cr>
2025-05-21 19:14:42 -0600

    Commit 1
    Mi primer commit

---
+++ Tienda_Ian/src/main/java/Tienda/demo/TiendaIanApplication.java
@@ -0,0 +1,13 @@
+package Tienda.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaIanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaIanApplication.class, args);
+	}
+
+}
